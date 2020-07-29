d66514026f84cbbe1f5dad9d0f83ede7a887d2bd
Sebastian Porling <sebastian.porling@gmail.com>
2020-07-29 16:37:36 +0200

    Update: The project structure task is done

---
+++ tasks/project_structure/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ted Gärdestad is the best swedish artist!");
+        System.out.println("Just to let you now...");
+    }
+}
